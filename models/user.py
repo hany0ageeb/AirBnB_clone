3505eef05de9c857bb7fbd93a67bfec4c5bb3d64
hany0ageeb <hany0ageeb@gmail.com>
2024-01-13 15:03:37 -0800

    change user.py

--- models/user.py
+++ models/user.py
@@ -7,9 +7,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """class User inherits from BaseModel"""
-    def __init__(self, *args, **kwargs):
-        self.email = ''
-        self.password = ''
-        self.first_name = ''
-        self.last_name = ''
-        super().__init__(*args, **kwargs)
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
