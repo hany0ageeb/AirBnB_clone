313e421394dd0ee26eeab445028d3701d1d1fdbe
hany0ageeb <hany0ageeb@gmail.com>
2024-01-14 03:59:25 -0800

    adding test file for onsole no tests so far

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines class TestConsole"""
+
+
+import unittest
+from console import HBNBCommand
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """defines test cases for console"""
+    pass
