6d3bbaa32116b6f962ebc2fa90939ecbf211024d
hany0ageeb <hany0ageeb@gmail.com>
2024-01-13 16:47:47 -0800

    Initial commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -8,23 +8,14 @@ from models.amenity import Amenity
 
 class TestAmenity(unittest.TestCase):
     """class TestAmenity to test Amenity class"""
-    def test_attr_name_exist(self):
-        """test Amenity name attribute exists"""
-        obj = Amenity()
+    def test_name_attr_exist(self):
+        """test class attribute name exist"""
         self.assertTrue(
-                hasattr(obj, 'name'),
-                msg='amenity name attribute not here')
-
-    def test_name_default_to_empty_str(self):
-        """tests name attribute is defaulted to emppty string"""
-        obj = Amenity()
+                hasattr(Amenity, 'name'),
+                msg='Amenity.name is not here!')
+    def test_name_attr_is_str(self):
+        """test class attribute name is of type str"""
         self.assertEqual(
-                obj.name,
-                '',
-                msg='amenity name should be empty')
-
-    def test_init_set_attributes_if_kwarg_present(self):
-        """test init passes kwargs to super()"""
-        obj = Amenity(name='xyz', id='123')
-        self.assertEqual(obj.name, 'xyz', msg='name should be xyz')
-        self.assertEqual(obj.id, '123', msg='id should be 123')
+                type(Amenity.name),
+                str,
+                msg='Amenity.name should be of type str!')
