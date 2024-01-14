ce6b48c607b06325af8c9235eed618ebdfef113c
hany0ageeb <hany0ageeb@gmail.com>
2024-01-13 17:30:26 -0800

    tests for FileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@
 
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
@@ -21,3 +22,29 @@ class TestFileStorage(unittest.TestCase):
         self.assertTrue(
                 hasattr(FileStorage, '_FileStorage__objects'),
                 msg='FileStorage.__objects is not here!')
+
+    def test_all_exist(self):
+        """test all instance method exist"""
+        obj = FileStorage()
+        self.assertTrue(
+                hasattr(obj, 'all'),
+                msg='instance method all does not exist!')
+
+    def test_all_returns__object(self):
+        """check all returns __objects dictionary"""
+        obj = FileStorage()
+        self.assertEqual(
+                FileStorage._FileStorage__objects,
+                obj.all(),
+                msg='all should return __objects')
+
+    def test_new_add_or_update_obj_in_objects(self):
+        """test new(self, obj): sets in __objects
+        the obj with key <obj class name>.id"""
+        storage = FileStorage()
+        obj = BaseModel()
+        obj.id = '123-123'
+        storage.new(obj)
+        self.assertTrue(
+                'BaseModel.123-123' in FileStorage._FileStorage__objects,
+                msg='new did not add obj to __objects')
