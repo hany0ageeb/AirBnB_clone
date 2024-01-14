2e1c4a7b186a3c92acbf2ddfe1746eae26fe4631
hany0ageeb <hany0ageeb@gmail.com>
2024-01-13 16:40:49 -0800

    Initial commit

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,38 +7,30 @@ from models.city import City
 
 class TestCity(unittest.TestCase):
     """class TestCity test City"""
-    def test_state_id_attr_exist(self):
-        """test state_id exist"""
-        obj = City()
+    def test_state_id_attr_exist_and_empty_initially(self):
+        """test state_id exist and initially empty"""
         self.assertTrue(
-                hasattr(obj, 'state_id'),
-                msg='state_id does not exist')
-
-    def test_state_id_default_empty_str(self):
-        """test state_id defaults to empty string"""
-        obj = City()
+                hasattr(City, 'state_id'),
+                msg='City.state_id is not here')
         self.assertEqual(
-                obj.state_id,
+                City.state_id,
                 '',
-                msg="state_id should be initially empty")
-
-    def test_name_attr_exist(self):
-        """test city name attribute exist"""
-        obj = City()
-        self.assertTrue(
-                hasattr(obj, 'name'),
-                msg='name does not exist')
-
-    def test_name_initially_empy(self):
-        """test city name initially empty string"""
-        obj = City()
-        self.assertEqual(obj.name, '', msg="city name should be empy")
-
-    def test_init_calling_BaseModel_init(self):
-        """test city.__init__ passing kwargs to BaseModel.__init__"""
-        obj = City(name='new york', state_id='123-123')
+                msg='City.state_id should be initally empty')
         self.assertEqual(
-                obj.name,
-                'new york',
-                msg='city name should be new york')
-        self.assertEqual(obj.state_id, '123-123', msg='city state_id not set')
+                type(City.state_id),
+                str,
+                msg='City.state_id should be str')
+
+    def test_name_attr_exist_and_not_empty_initially(self):
+        """test name attr is exist and is empty string"""
+        self.assertTrue(
+                hasattr(City, 'name'),
+                msg='City.name is not here')
+        self.assertEqual(
+                type(City.name),
+                str,
+                msg='City.name is not of str type')
+        self.assertEqual(
+                City.name,
+                '',
+                msg='City.name should be initially empty string')
