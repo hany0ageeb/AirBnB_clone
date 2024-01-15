78831cf4fd178aec9b9d6c30a450038a94a3d9b0
hany0ageeb <hany0ageeb@gmail.com>
2024-01-15 03:11:09 -0800

    add tests to test_console for quit is present

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,4 +8,10 @@ from console import HBNBCommand
 
 class TestHBNBCommand(unittest.TestCase):
     """defines test cases for console"""
-    pass
+
+    def test_quit_present(self):
+        """tests do_quit present"""
+        # setup
+        instance = HBNBCommand()
+        # test
+        self.assertTrue(hasattr(instance, 'do_quit'))
